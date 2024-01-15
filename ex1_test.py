10800f897eda0a4a8be7ee28fcbeb222066c1387
AbdBitar <abd.bitar.98@gmail.com>
2024-01-14 21:02:56 -0500

    first commit

---
+++ ex1_test.py
@@ -0,0 +1,8 @@
+import write_your_name as name_point
+
+def test_hi_my_name_is_kareem():
+    assert len(name_point.hi_my_name_is_kareem()) > 1
+
+
+if __name__ == "__main__":
+    test_hi_my_name_is_kareem()
---
+++ write_your_name.py
@@ -0,0 +1,6 @@
+def hi_my_name_is_kareem():
+    return "Kareem"
+
+
+if __name__ == "__main__":
+    print(hi_my_name_is_kareem())
